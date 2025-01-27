7447810ecdaadba42c610ec321b408b7400a4a83
Francis Grizzly Smit <grizzlysmit@smit.id.au>
2025-01-27 15:01:49 +1100

    commited with save.raku 2025-01-27T15:01:49.641860+11:00

--- prefs.js
+++ prefs.js
@@ -747,11 +747,11 @@ export default class MyPreferences extends ExtensionPreferences {
             switch(this.page_name){
                 case("settings"):
                     this.page = this._pageNotesPreferencesSettings;
-                    window.set_visible_page(this.page);
+                    this._window.set_visible_page(this.page);
                     break;
                 case("notesScroller"):
                     this.page = this._NotesScroller;
-                    window.set_visible_page(this.page);
+                    this._window.set_visible_page(this.page);
                     break;
                 case("editNote"):
                     this.page = this._EditNote;
@@ -759,15 +759,15 @@ export default class MyPreferences extends ExtensionPreferences {
                     break;
                 case("aboutPage"):
                     this.page = this.aboutPage;
-                    window.set_visible_page(this.page);
+                    this._window.set_visible_page(this.page);
                     break;
                 case("credits"):
                     this.page = this.creditsPage;
-                    window.set_visible_page(this.page);
+                    this._window.set_visible_page(this.page);
                     break;
                 default:
                     this.page = this._pageNotesPreferencesSettings;
-                    window.set_visible_page(this.page);
+                    this._window.set_visible_page(this.page);
             }  // switch(this.page_name) //
         } // if(this.edit_note) //
         this.settingsID_page       = this._window._settings.connect("changed::page", this.onPageChanged.bind(this));
@@ -780,7 +780,7 @@ export default class MyPreferences extends ExtensionPreferences {
     editNote(_index){
         this.page = this._EditNote;
         this._EditNote.set_index(_index);
-        window.set_visible_page(this.page);
+        this._window.set_visible_page(this.page);
     } // editNote(_index) //
 
     _close_request(_win){
