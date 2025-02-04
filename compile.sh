84771a2aae521c9f6c1551206f943bda4e263600
Francis Grizzly Smit <grizzlysmit@smit.id.au>
2025-02-05 10:34:46 +1100

    commited with save.raku 2025-02-05T10:34:46.068781+11:00

--- compile.sh
+++ compile.sh
@@ -5,7 +5,7 @@ glib-compile-schemas schemas
 DIR="po"
 SUFFIX="po"
 
-xgettext --from-code='utf-8' -k_ -kN_ -o $DIR/noteswithhistory.pot prefs.js extension.js gzz.js
+xgettext --from-code='utf-8' -k_ -kN_ -o $DIR/notes-with-history.pot prefs.js extension.js gzz.js
 
 for file in "$DIR"/*.$SUFFIX
 do 
@@ -13,5 +13,5 @@ do
 	lingua=${lingua#*/}
 	msgfmt $file
 	mkdir locale/$lingua/LC_MESSAGES -p
-	mv messages.mo locale/$lingua/LC_MESSAGES/noteswithhistory.mo
+	mv messages.mo locale/$lingua/LC_MESSAGES/notes-with-history.mo
 done
