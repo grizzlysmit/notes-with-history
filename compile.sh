29ffa4a0e31738d559a98a9be9f41ad1c8229d84
Francis Grizzly Smit <grizzly@smit.id.au>
2025-07-23 14:44:41 +1000

    commited with save.raku 2025-07-23T14:44:41.782088+10:00

--- compile.sh
+++ compile.sh
@@ -5,7 +5,17 @@ glib-compile-schemas schemas
 DIR="po"
 SUFFIX="po"
 
-xgettext --from-code='utf-8' -k_ -kN_ -o $DIR/notes-with-history.pot prefs.js extension.js
+xgettext --from-code='utf-8' -k_ -kN_ -o $DIR/notes-with-history.pot extension.js prefs.js
+
+if ! [ -x "$DIR"/en.$SUFFIX ]
+then
+    msginit --input="$DIR/notes-with-history.pot" --output-file="$DIR/en.po" --no-translator
+else
+    for file in "$DIR"/*.$SUFFIX 
+    do
+        msgmerge --previous --update "$DIR/$file" "$DIR/notes-with-history.pot"
+    done
+fi
 
 for file in "$DIR"/*.$SUFFIX
 do 
