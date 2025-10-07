dd0f12db12614e5a3279ba13ccfdba29295b9b83
Francis Grizzly Smit <grizzly@smit.id.au>
2025-10-07 19:53:31 +1100

    commited with save.raku 2025-10-07T19:53:31.006263+11:00

--- extension.js
+++ extension.js
@@ -475,9 +475,7 @@ class Indicator extends PanelMenu.Button {
         this.loadMesessages();
         const area               = this.areas[this.settings.get_enum("area")];
         LogMessage.log_message(LogMessage.get_prog_id(), `area == ${area}`, new Error());
-        if(Main.panel.statusArea[this._caller._name]){
-            Main.panel.statusArea[this._caller._name] = null;
-        }
+        Main.panel.statusArea[this._caller._name] = null; // in case it exists already //
         Main.panel.addToStatusArea(this._caller._name, this, this.settings.get_int("position"), area);
 
         this.settingsID_show     = this.settings.connect("changed::show-messages", () => {
